be6ebbf0db78d79ae4bbdd173193318018424dd7
AzureAlexis <alex.hamilton2025@gmail.com>
2023-06-19 12:47:11 -0400

    6/18/23
    Un-spagettified code

--- script.js
+++ script.js
@@ -3,11 +3,27 @@
 	It also creates contains a few functions that make displaying text easier and more *stylish*
 */
 
-// The whole damn script
+// The max character width for each textbox line
 const LINEWIDTH = 50;
-var waiting = false;
-var start;
-var end;
+
+/*
+	The story array indicates where the player is in the plot in each map
+	The for loop right below it sets all values from 0-999 to 0, initilizing the array
+*/
+var story = [];
+for(i = 0; i < 1000; i++) {
+	story[i] = 0;
+}
+
+/* 
+	The whole damn script, contained in a 3d array
+	
+	x diam:	The map to pull text for
+	y diam: The line in the selected map's script to pull
+	z diam: Used only if the selected x and y coordinates contain a choice
+			The string in this diam contains the text to be displayed
+			The array contains the choices to display.
+*/
 var script = [
 	[
 		["", ["Skill", "Item", "Equip", "Info", "System"]]
@@ -26,49 +42,74 @@ var script = [
 		"You equipped FADED DICE!",
 		"You got STRANGE KEYRING!",
 		"You equipped STRANGE KEYRING!",
-		"You couldn't open the door. It's not locked, but Esper refuses to open it",
+		"You couldn't open the door.  It's not locked, but Esper refuses to open it",
 		"You hear a faint click",
-		["A shadowy door stands before you", ["Open", "=>"]]
+		["", ["Open", "=>"]]
 	]
 ];
 
-var story = [];
-for(i = 0; i < 1000; i++) {
-	story[i] = 0;
+/*
+	runChoice: Contains all possible outcomes of all choices in the script
+	
+	@param	x: The map to pull from
+			y: The script line to pull from
+			n: Which choice the player selected
+	@return	void
+	
+*/
+function runChoice(x, y, n) {
+	// console.log(x + " " + y + " " + n); // Debug use only
+	switch(x) {
+	case 1:
+		switch(x) {
+		case 16:
+			switch(n) {
+			case 0:
+				movePlayer(8, 6, 2, 0);
+			case 1:
+				mapText(0, 0);
+			}
+		}
+	}
 }
 
-function formatText(x, y = 0) {
+/* 
+	Formats text before printing it, adding line breaks and "Esper: " at the end
+	If the passed string already has line breaks, the function doesn't add more
+	
+	@param	x: The string to Formats
+	@return	The formatted string
+*/
+function formatText(x) {
 	let output = x;
+	
 	if(x.indexOf("\n") == -1) {
 		for(let i = LINEWIDTH; i < x.length; i = i + LINEWIDTH) {
 			output = output.substring(0, i + (i / LINEWIDTH * 2) - 2) + "\n" + output.substring(i + (i / LINEWIDTH * 2) - 2);
 		}
-	}
-	for(let i = output.length; i < 150; i = i + LINEWIDTH) {
+		
+		for(let i = output.length; i < 150; i = i + LINEWIDTH) {
 		output = output + "\n";
+		}
 	}
+	
 	output = output + "\\c[8]Esper:";
 	
 	return output;
 }
 
-function runChoice(x, y, n) {
-	console.log(y);
-	switch(y) {
-	case 1:
-		console.log(x);
-		switch(x) {
-		case 16:
-			movePlayer(8, 6, 2, 0);
-			break;
-		}
-	}
-}
-
-// Formats text and shows it with the in game windows
-// If the parameter is an array, a choice is presented instead
+/*
+	Displays a single line of text, pulled from the chosen map's script
+	If the selected line contains a choice, executes runChoice based on selected choice and params
+	
+	@param	x: The line to pull from the script of the selected map
+			y: The map to pull script from (Default: Current map)
+	@return	void
+*/
 function mapText(x, y = $gameMap._mapId) {
 	let z = script[y][x];
+	
+	$gameMessage.newPage();
 	
 	$gameMessage.setBackground(2);
 	$gameMessage.setPositionType(2);
@@ -78,14 +119,25 @@ function mapText(x, y = $gameMap._mapId) {
 	if(Array.isArray(z)) {
 		let z2 = z[1];
 		$gameMessage.add(formatText(z[0]), 1);
-		$gameMessage.setChoices(z2, 0);
+		$gameMessage.setChoices(z2, 0, -2);
 		$gameMessage.setChoiceCallback(function(n){runChoice(x, y, n)});
 	} else {
 		$gameMessage.add(formatText(z));
-		$gameMessage.setChoices(["=>"], 0);
+		$gameMessage.setChoices(["=>"], 0, -2);
 	}
 	$gameMap._interpreter.setWaitMode('message');
+	
+	return 
 }
+
+/*
+	Displays one or more lines of text, by repeatadly calling mapText
+	
+	@param	x: Position to start at
+			y: Position to end at (Default: x)
+*/
+var start;
+var end;
 function massText(x, y = x) {
 	$gameVariables.setValue(3, x);
 	$gameVariables.setValue(4, y);
@@ -93,13 +145,34 @@ function massText(x, y = x) {
 	$gameMap._interpreter.command117();
 }
 
-function storyText(x, y = x) {
-	if(story[$gameMap._mapId] < x) {
-		massText(x, y);
-		story[$gameMap._mapId] = x;
+/*
+	Displays one or more lines of text if x is greater than the story value of the selected map
+	If text is displayed, sets the value of the checked story value to y
+	
+	@param	x: The number to check the story value against. If true, text is displayed starting at x
+			y: The value the checked story value is set to if true, and the last line of text to display if true (Default: x)
+			z: The map to check the story value of (Default: Current map)
+	@return 
+*/
+function storyText(x, y = x, z = $gameMap._mapId) {
+	if(story[z] < x) {
+		massText(x, y, z);
+		story[z] = y;
+		return true;
+	} else {
+		return false;
 	}
 }
 
-function storyTick(x = 1) {
-	story[$gameMap._mapId] = story[$gameMap._mapId] + x;
+/*
+	Changes the player's story value
+	If no params are passed, increases the current map's story value by 1
+	
+	@param	x: Amount to change the selected story value by (Default: 1)
+			y: Which map to change the story value of (Default: Current map)
+	@return	The modified story value
+*/
+function storyTick(x = 1, y = $gameMap._mapId) {
+	story[y] = story[y] + x;
+	return story[y];
 }
--- windows.js
+++ windows.js
@@ -1,98 +1,103 @@
-// Controls custom window functions
-// Credit to Shiko for help with horizontol choice positioning
+/*	
+	Controls custom window functions, by overwriting native JS functions and adding new ones
+	Credit to Shiko for help with horizontol choice positioning
+*/ 
 
-Window_ChoiceList.prototype.updatePlacement = function() { 
-	var messageY = this._messageWindow.y;
-	this.width = this.windowWidth();
-	this.height = this.windowHeight();
-	
-    if ($gameMessage.choicePositionType() == 1) {
-		this.x = 112;
-		this.y = 408;
-	} else {
-		this.x = Graphics.boxWidth - this.width;
-		if (messageY >= Graphics.boxHeight / 2) {
-			this.y = messageY - this.height;
-		} else {
-			this.y = messageY + this._messageWindow.height;
-		}
-	}
-}
-
-Window_ChoiceList.prototype.drawItem = function(index) {
-	if ($gameMessage.choicePositionType() == 1) {
-		var rect = this.itemRectForText(index);
-		var text = this.commandName(index);
-		this.drawTextEx(text, rect.x, rect.y);
-	} else {
-		var rect = this.itemRectForText(index);
-		this.drawTextEx(this.commandName(index), rect.x, rect.y);
-	}
-}
-
-Window_ChoiceList.prototype.numVisibleRows = function() {
-	if ($gameMessage.choicePositionType() == 1) {
-		return 1;
-	} else {
+// Overwrites default window behavior
+(function() {
+	Window_ChoiceList.prototype.updatePlacement = function() { 
 		var messageY = this._messageWindow.y;
-		var messageHeight = this._messageWindow.height;
-		var centerY = Graphics.boxHeight / 2;
-		var choices = $gameMessage.choices();
-		var numLines = choices.length;
-		var maxLines = 8;
-		if (messageY < centerY && messageY + messageHeight > centerY) {
-			maxLines = 4;
+		this.width = this.windowWidth();
+		this.height = this.windowHeight();
+		
+		if ($gameMessage.choicePositionType() == 1) {
+			this.x = 112;
+			this.y = 408;
+		} else {
+			this.x = Graphics.boxWidth - this.width;
+			if (messageY >= Graphics.boxHeight / 2) {
+				this.y = messageY - this.height;
+			} else {
+				this.y = messageY + this._messageWindow.height;
+			}
 		}
-		if (numLines > maxLines) {
-			numLines = maxLines;
-		}
-		return numLines;
 	}
-}
 
-Window_ChoiceList.prototype.maxCols = function() {
-	if ($gameMessage.choicePositionType() == 1) {
-		var choices = $gameMessage.choices();
-		numCols = choices.length;
-		switch (numCols) {
-		case 1:
+	Window_ChoiceList.prototype.drawItem = function(index) {
+		if ($gameMessage.choicePositionType() == 1) {
+			var rect = this.itemRectForText(index);
+			var text = this.commandName(index);
+			this.drawTextEx(text, rect.x, rect.y);
+		} else {
+			var rect = this.itemRectForText(index);
+			this.drawTextEx(this.commandName(index), rect.x, rect.y);
+		}
+	}
+
+	Window_ChoiceList.prototype.numVisibleRows = function() {
+		if ($gameMessage.choicePositionType() == 1) {
 			return 1;
-		case 2:
-			return 2;
-		case 3:
-			return 3;
-		case 4:
-			return 4;
-		case 5:
-			return 5;
-		case 6:
-			return 6;
+		} else {
+			var messageY = this._messageWindow.y;
+			var messageHeight = this._messageWindow.height;
+			var centerY = Graphics.boxHeight / 2;
+			var choices = $gameMessage.choices();
+			var numLines = choices.length;
+			var maxLines = 8;
+			if (messageY < centerY && messageY + messageHeight > centerY) {
+				maxLines = 4;
+			}
+			if (numLines > maxLines) {
+				numLines = maxLines;
+			}
+			return numLines;
 		}
-	} else {
-		return 1;
 	}
-}
 
-Window_ChoiceList.prototype.windowWidth = function() {
-	if ($gameMessage.choicePositionType() == 1) {
+	Window_ChoiceList.prototype.maxCols = function() {
+		if ($gameMessage.choicePositionType() == 1) {
+			var choices = $gameMessage.choices();
+			numCols = choices.length;
+			switch (numCols) {
+			case 1:
+				return 1;
+			case 2:
+				return 2;
+			case 3:
+				return 3;
+			case 4:
+				return 4;
+			case 5:
+				return 5;
+			case 6:
+				return 6;
+			}
+		} else {
+			return 1;
+		}
+	}
+
+	Window_ChoiceList.prototype.windowWidth = function() {
+		if ($gameMessage.choicePositionType() == 1) {
+			var choices = $gameMessage.choices();
+			var numLines = choices.length;
+			var width = (this.maxChoiceWidth() + 10 * 2)*choices.length;
+			return Math.min(width, Graphics.boxWidth*2);
+		} else {
+			var width = this.maxChoiceWidth() + 10 * 2;
+			return Math.min(width, Graphics.boxWidth);
+		}
+	}
+
+	Window_ChoiceList.prototype.maxChoiceWidth = function() {
+		var maxWidth = 60;
 		var choices = $gameMessage.choices();
-		var numLines = choices.length;
-		var width = (this.maxChoiceWidth() + 10 * 2)*choices.length;
-		return Math.min(width, Graphics.boxWidth*2);
-	} else {
-		var width = this.maxChoiceWidth() + 10 * 2;
-		return Math.min(width, Graphics.boxWidth);
-	}
+		for (var i = 0; i < choices.length; i++) {
+			var choiceWidth = this.textWidthEx(choices[i]) + 10 * 2;
+			if (maxWidth < choiceWidth) {
+				maxWidth = choiceWidth;
+			}
+		}
+		return maxWidth;
+	};
 }
-
-Window_ChoiceList.prototype.maxChoiceWidth = function() {
-    var maxWidth = 60;
-    var choices = $gameMessage.choices();
-    for (var i = 0; i < choices.length; i++) {
-        var choiceWidth = this.textWidthEx(choices[i]) + 10 * 2;
-        if (maxWidth < choiceWidth) {
-            maxWidth = choiceWidth;
-        }
-    }
-    return maxWidth;
-};
